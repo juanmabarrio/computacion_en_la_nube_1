91461a99b2deedab95cae7fef9f14e68d700c862
Juanmeta <juanmabarrio@gmail.com>
2020-02-16 14:15:36 +0100

    cambios

--- src/main/java/s3/controller/BucketsRestController.java
+++ src/main/java/s3/controller/BucketsRestController.java
@@ -3,16 +3,16 @@ package s3.controller;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
+import java.io.File;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,7 +26,7 @@ public class BucketsRestController {
     @GetMapping("/")
     public ResponseEntity<List<String>> getAllBuckets() {
         List<String> bucketNames = s3.listBuckets().stream().map(Bucket::getName).collect(Collectors.toList());
-        if ( bucketNames == null || bucketNames.size()==0 ) {
+        if (bucketNames == null || bucketNames.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(bucketNames, HttpStatus.OK);
@@ -58,12 +58,34 @@ public class BucketsRestController {
         if (s3.doesBucketExistV2(bucketName))
             throw new EntityExistsException();
         Bucket newBucket = s3.createBucket(bucketName);
-        return new ResponseEntity<>(newBucket,HttpStatus.OK);
+        return new ResponseEntity<>(newBucket, HttpStatus.OK);
+    }
+
+    @PostMapping("/{bucketName}/uploadObject")
+    public ResponseEntity uploadObject(@PathVariable String bucketName, @RequestBody String fileContent) {
+        if (!s3.doesBucketExistV2(bucketName))
+            throw new EntityNotFoundException();
+        Bucket newBucket = s3.createBucket(bucketName);
+        PutObjectRequest por = new PutObjectRequest(
+                bucketName,
+                createRandomFileName(),
+                fileContent
+        );
+        por.setCannedAcl(CannedAccessControlList.PublicRead);
+        s3.putObject(por);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 
-
-
-
+    private String createRandomFileName() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 
 }
